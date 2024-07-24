d05c5c2cb6a382fe1eddbbd1e855b1bc6c3aafb7
Farzeen Zainab <farzeenzaidi51@gmail.com>
2024-07-24 06:13:22 +0500

    implemening redux thunk to run async code in the slice

--- src/App.js
+++ src/App.js
@@ -1,40 +1,81 @@
-import { useSelector } from "react-redux";
+import { useEffect } from "react";
+
+import { useDispatch, useSelector } from "react-redux";
 
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
-import { useEffect } from "react";
+
+import { sendCartData } from "./store/cart-slice";
+import Notification from "./components/UI/Notificiation";
+import { uiActions } from "./store/ui-slice";
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
   const cart = useSelector((state) => state.cart);
+  const notification = useSelector((state) => state.ui.notification);
+  const dispatch = useDispatch();
 
   // sending an async request to firebase when the state of our cart changes
   // There is a problem with this approach. When the page loads for the first time
   // useEffect will send the request with empty body that will overwrite the existing
   // cart data in firebase
+  // useEffect(() => {
+  //   const sendCartData = async () => {
+  //     const response = await fetch(
+  //       "https://redux-thunk-9110c-default-rtdb.firebaseio.com/cart.json",
+  //       {
+  //         method: "PUT",
+  //         body: JSON.stringify(cart),
+  //       }
+  //     );
+
+  //     if (!response.ok) {
+  //       throw new Error("Sending cart data failed");
+  //     }
+
+  //     return await response.json();
+  //   };
+
+  //   sendCartData();
+  // }, [cart]);
+
+  // sending request and performing async task using redux thunk
   useEffect(() => {
-    const sendCartData = async () => {
-      const response = await fetch(
-        "https://redux-thunk-9110c-default-rtdb.firebaseio.com/cart.json",
-        {
-          method: "PUT",
-          body: JSON.stringify(cart),
-        }
+    // disptach not only takes action types and payload as argurments but
+    // it also take custom action creators thunks that takes functions
+    // it disptach sees that an action creator that returns a function is
+    // passed instead of an action type object, it will automatically execute
+    // that function
+    // Because it is a complex senerior, disptach is again provided in the parameter
+    // of the redux thunk, so we can disptach again from inside the thunk
+    dispatch(sendCartData(cart));
+
+    const timeout = setTimeout(() => {
+      dispatch(
+        uiActions.showNotification({
+          title: "",
+          message: "",
+          status: "",
+        })
       );
+    }, 3000);
 
-      if (!response.ok) {
-        throw new Error("Sending cart data failed");
-      }
-
-      return await response.json();
+    return () => {
+      clearTimeout(timeout);
     };
-
-    sendCartData();
-  }, [cart]);
+  }, [cart, dispatch]);
 
   return (
     <Layout>
+      {notification && (
+        <Notification
+          status={notification?.status}
+          title={notification?.title}
+          message={notification?.message}
+        />
+      )}
+
       {showCart && <Cart />}
       <Products />
     </Layout>
--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { uiActions } from "./ui-slice";
 
 const cartSlice = createSlice({
   name: "cart",
@@ -43,8 +44,41 @@ const cartSlice = createSlice({
 
 // action creator thunks
 
-const sendCartData = (cartData) => {
-  return { type: "", payload: "" };
+// default
+// const sendCartData = (cartData) => {
+//   return { type: "", payload: "" };
+// };
+
+// thunk
+export const sendCartData = (cart) => {
+  return async (dispatch) => {
+    //we can run any async code and preform side effects because we have
+    // not reached our reducer
+
+    try {
+      const response = await fetch(
+        "https://redux-thunk-9110c-default-rtdb.firebaseio.com/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Sending cart data failed");
+      }
+
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success!",
+          message: "Cart data sent successfully!",
+        })
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
 };
 
 export const cartActions = cartSlice.actions;
